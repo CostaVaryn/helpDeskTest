e2d1b9a4a26a5266945c2e95d5926c1a05e6eead
Costa Varyn <d3tug@yandex.ru>
2022-08-10 11:56:45 +0300

    add last pack

---
+++ src/test/java/helpers/TestValues.java
@@ -0,0 +1,7 @@
+package helpers;
+
+public class TestValues {
+    public static String TEST_BODY = "Message to technical support using selenium";
+    public static String TEST_TITLE = "Learning java";
+    public static String TEST_EMAIL = "test1@email.com";
+}
