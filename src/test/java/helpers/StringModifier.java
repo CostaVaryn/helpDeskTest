c0a8fe6cfdba9e09f62cf206e69e0f1538d0e910
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-08-12 15:11:58 +0300

    Update StringModifier.java

--- src/test/java/helpers/StringModifier.java
+++ src/test/java/helpers/StringModifier.java
@@ -6,6 +6,5 @@ import java.util.Date;
 public class StringModifier {
     public static String getUniqueString(String str){
         return str + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
     }
 }
